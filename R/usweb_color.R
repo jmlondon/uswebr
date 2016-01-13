bbc2930060a5e44bb45e5a5c9d3f39895824d4ef
Josh London <josh.london@noaa.gov>
2016-01-13 13:36:40 -0800

    REMOVE COLOR PALETTES FROM THE PACKAGE
    re-focus the package on just the rmarkdown report template and the ggplot theme

--- R/usweb_color.R
+++
@@ -1,7 +0,0 @@
-#' Return color value (hex) for a color described in the US Web Design Standards
-#'
-#' @param color_name Color name
-#' @export
-usweb_color <- function(color_name) {
-  usweb_colors[[color_name]]
-}
